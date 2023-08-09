a80daecd109794edfecb3efdf6a4171cfd8fff60
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2023-08-09 11:14:40 +0200

    switch to openai function

--- src/bia_bob/_machinery.py
+++ src/bia_bob/_machinery.py
@@ -1,7 +1,6 @@
 class _context():
     tools = []
     llm = None
-    chat_agent = None
     agent = None
     variables = None
     verbose = False
@@ -38,39 +37,36 @@ def bob(line: str = None, cell: str = None):
 def init_assistant(variables, temperature=0):
     if _context.verbose:
         print("Initializing assistant")
-    from langchain.memory import ConversationBufferMemory
+    from langchain import OpenAI, LLMChain
     from langchain.chat_models import ChatOpenAI
-    from langchain.agents import initialize_agent
-    from langchain.agents import AgentType
+    from langchain.memory import ConversationBufferMemory
+    from langchain.agents import StructuredChatAgent, AgentExecutor, OpenAIFunctionsAgent, initialize_agent, AgentType
+    from langchain.prompts import MessagesPlaceholder
+    from langchain.schema import SystemMessage
 
     if len(_context.tools) == 0:
         from ._tools import load_image
 
-    _context.memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    # make the llm
+    _context.llm = ChatOpenAI(temperature=temperature)
 
-    # set up the prompt
-    prefix = """Have a conversation with a human, answering the following questions as best you can. You have access to the following tools:"""
-    suffix = """Begin!"
+    # set up the chat memory
+    MEMORY_KEY = "chat_history"
+    _context.memory = ConversationBufferMemory(memory_key=MEMORY_KEY, return_messages=True)
 
-    {chat_history}
-    Question: {input}
-    {agent_scratchpad}"""
-
-    prompt = StructuredChatAgent.create_prompt(
-        tools,
-        prefix=prefix,
-        suffix=suffix,
-        input_variables=["input", "chat_history", "agent_scratchpad"],
+    system_message = SystemMessage(content="You are a powerful assistant. Answer the human's questions below.")
+    agent_kwargs = {
+        "system_message": system_message,
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name=MEMORY_KEY)],
+    }
+    _context.agent = initialize_agent(
+        _context.tools,
+        _context.llm,
+        agent=AgentType.OPENAI_FUNCTIONS,
+        verbose=_context.verbose,
+        agent_kwargs=agent_kwargs,
+        memory=_context.memory,
     )
-    memory = ConversationBufferMemory(memory_key="chat_history")
-
-    # make the agent executor
-    _context.llm = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
-    _context.chat_agent = StructuredChatAgent(llm_chain=llm_chain, tools=tools, verbose=True)
-    _context.agent = AgentExecutor.from_agent_and_tools(
-        agent=agent, tools=tools, verbose=True, memory=memory
-    )
-
     # store the variables
     _context.variables = variables
 
