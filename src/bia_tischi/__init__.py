66d23f489e19794e9ed0a9b2828e878d0bb48d42
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-09-14 14:27:37 +0200

    removed unused code

--- src/bia_tischi/__init__.py
+++ src/bia_tischi/__init__.py
@@ -3,9 +3,8 @@ __version__ = "0.1.0"
 __all__ = (
     )
 
-from ._machinery import tischi, init_assistant, add_function_tool
+from ._machinery import tischi, init_assistant
 
 tischi.initialize = init_assistant
-tischi.add_function_tool = add_function_tool
 
 tischi.__version__ = __version__
