d0e74793be2c0f45989e1ff0aa01e81839763b7a
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-10-04 15:54:40 +0200

    renamed ai to bob

--- src/bia_tischi/__init__.py
+++ src/bia_tischi/__init__.py
@@ -3,7 +3,7 @@ __version__ = "0.1.0"
 __all__ = (
 )
 
-from ._machinery import ai, init_assistant
+from ._machinery import bob, init_assistant
 
-ai.initialize = init_assistant
-ai.__version__ = __version__
+bob.initialize = init_assistant
+bob.__version__ = __version__
