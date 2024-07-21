1aa48284e163724e0f1ce91c586c2abccb1c195f
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-07-21 08:20:42 +0200

    add hints for pandas

---
+++ src/bia_bob/suggestions/_pandas.py
@@ -0,0 +1,7 @@
+def suggestions():
+    return """
+    ### Working with Pandas DataFrames
+
+    In case a pandas DataFrame, e.g. `df` is the result of a code block, just write `df.head()`
+    by the end so that the user can see the intermediate result.
+    """
